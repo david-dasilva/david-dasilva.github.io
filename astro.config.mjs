fd0d932302abb04d9c04f0c996af5d505fe4c0eb
David Da Silva <david.dasilva@zenika.com>
2025-06-11 22:32:11 +0200

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://david-dasilva.github.io',
+  base: 'david-dasilva.github.io',
+  trailingSlash: "always",
   integrations: [tailwind()],
 });
